fb6527d4d3681712519785032b58ef1bf78306ad
Jiří Žán <jirka@jizan.cz>
2021-10-19 09:38:24 +0200

    Indikátor - unRead

--- src/components/Conversations.tsx
+++ src/components/Conversations.tsx
@@ -19,22 +19,23 @@ export const Conversations = (props: Props) => {
         return <div className="card-content py-2">There is nothing to display</div>
     }
 
-    return <>{filteredConversations.map(row => {
-        const toPersonId = row.users.find(id => id !== props.loggedUserId);
+    return <>{filteredConversations.map(conversation => {
+        const toPersonId = conversation.users.find(id => id !== props.loggedUserId);
         const toPerson = props.people.find(person => person.id === toPersonId);
-        const lastMessage = row.messages.length ? row.messages[row.messages.length - 1] : null;
+        const lastMessage = conversation.messages.length ? conversation.messages[conversation.messages.length - 1] : null;
+        const isUnread = conversation.messages.find(row => (row.from === toPersonId) && !row.displayed)
 
         return <Conversation
-            key={row.id}
+            key={conversation.id}
             initials={getInitials(toPerson ? toPerson.name : '', toPerson ? toPerson.surname : '')}
             name={toPerson ? toPerson.name : ''}
             surname={toPerson ? toPerson.surname : ''}
             lastLine={lastMessage ? lastMessage.text : ''}
-            active={props.activeConversationId === row.id}
-            unread={lastMessage ? !lastMessage.displayed : false}
+            active={props.activeConversationId === conversation.id}
+            unread={!!isUnread}
             time={lastMessage ? DateTime.fromISO(lastMessage.sent).toFormat('h:mm a') : ''}
             onClick={e => {e.preventDefault(); props.dispatch({type: 'setActiveConversation', value: {
-                activeConversationId: row.id,
+                activeConversationId: conversation.id,
                 activePersonId: toPersonId
             }})}}
     />})}</>
